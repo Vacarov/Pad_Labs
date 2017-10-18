f0ccdd8c8d95efc2013f6d8bf62d5934cd26a512
Verginiu VACAROV <vvacarov@pentalog.com>
2017-10-18 12:12:41 +0300

    task_multiThreading => create ClientConnection class, need to deplace queue in Client Connection

--- src/main/java/labs/broker/Broker.java
+++ src/main/java/labs/broker/Broker.java
@@ -25,38 +25,24 @@ public class Broker extends Thread {
     public void start() {
         Queue<Message> queue = null;
         QueueStorage queueStorage = new QueueStorage();
+
         if (!queueStorage.isEmptyQueue()) {
             queue = queueStorage.restoreQueue();
             System.out.println("Queue is restored:\n " + queue.toString());
-        } else queue = new ConcurrentLinkedQueue<Message>();
+        } else queue = new ConcurrentLinkedQueue<>();
 
         queueStorage.storeQueueWhenApplicationStopped((ConcurrentLinkedQueue<Message>) queue);
         System.out.println("I am waiting for clients...");
         try {
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                System.out.println("Hello client with port " + clientSocket.getPort());
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                String input;
 
-                while ((input = in.readLine()) != null) {
-                    MessageTransformer messageTransformer = new MessageTransformer();
-                    Message message = messageTransformer.transformFromGson(input);
-                    if (message.getOrder() == MessageOrder.SEND) {
-                        queue.add(message);
-                    } else {
-                        if (queue.isEmpty()) {
-                            System.out.println("No more messages in queue");
-                            break;
-                        } else {
-                            message = queue.poll();
-                            final String json = messageTransformer.transformIntoGson(message);
-                            out.println(json);
-                        }
-                    }
-                    System.out.println(queue);
-                }
+                ConcurrentLinkedQueue<Message> finalQueue = (ConcurrentLinkedQueue<Message>) queue;
+                new Thread(() -> {
+//                    ClientConnection clientConnection = new ClientConnection(clientSocket).start(finalQueue);
+                                    }).start();
+//                ClientConnection clientConnection = new ClientConnection(clientSocket);
+//                clientConnection.start((ConcurrentLinkedQueue<Message>) queue);
             }
         } catch (Exception e) {
             e.printStackTrace();
